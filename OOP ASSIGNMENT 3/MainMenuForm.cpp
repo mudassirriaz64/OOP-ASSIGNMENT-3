189ae207ef49604841f3185d84ef6ab0b5bab51b
msamishahid <146338536+msamishahid@users.noreply.github.com>
2024-05-25 17:44:21 +0500

    Update MainMenuForm.cpp

--- OOP ASSIGNMENT 3/MainMenuForm.cpp
+++ OOP ASSIGNMENT 3/MainMenuForm.cpp
@@ -1,6 +1,6 @@
 #include "MainMenuForm.h"
 #include <Windows.h>
-#include <cstdlib> // Include the <cstdlib> header for the exit function
+#include <cstdlib>
 
 using namespace System;
 using namespace System::Windows::Forms;
@@ -8,10 +8,10 @@ using namespace System::Windows::Forms;
 int main()
 {
     String^ connectionString = "Server=DESKTOP-MN4CFP4;Database=TimeTableDB;Integrated Security=True;";
-    Application::EnableVisualStyles(); // Enable visual styles before running the application
-    Application::SetCompatibleTextRenderingDefault(false); // Set compatibility with default text rendering
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false); 
 
-    Application::Run(gcnew OOPASSIGNMENT2::MainMenuForm()); // Qualify the MainMenuForm with its namespace
+    Application::Run(gcnew OOPASSIGNMENT2::MainMenuForm());
 
-    return EXIT_SUCCESS; // Use EXIT_SUCCESS from cstdlib for portability
+    return EXIT_SUCCESS;
 }
