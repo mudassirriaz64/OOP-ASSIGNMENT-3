2fec3dd00a069fe98ac20a42ba7870033f295437
msamishahid <146338536+msamishahid@users.noreply.github.com>
2024-05-25 17:31:55 +0500

    Update DisplayStudents.h

--- OOP ASSIGNMENT 3/DisplayStudents.h
+++ OOP ASSIGNMENT 3/DisplayStudents.h
@@ -1,93 +1,110 @@
-#include <fstream>
 #include <string>
-#include <vector>
 
 namespace OOPASSIGNMENT2
 {
-    using namespace System;
-    using namespace System::ComponentModel;
-    using namespace System::Collections;
-    using namespace System::Windows::Forms;
-    using namespace System::Data;
-    using namespace System::Drawing;
-    using namespace System::IO;
-    using namespace System::Collections::Generic; // Add this for List<T>
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace System::IO;
+	using namespace System::Collections::Generic;
+	using namespace System::Data::SqlClient;
 
-    // Define a class to represent a student
-    ref class Student {
-    public:
-        String^ EnrollmentID;
-        String^ Name;
-        String^ Section;
-    };
 
-    /// <summary>
-    /// Summary for DisplayStudents
-    /// </summary>
-    public ref class DisplayStudents : public System::Windows::Forms::Form
-    {
-    public:
-        DisplayStudents(void)
-        {
-            InitializeComponent();
-            PopulateDataGridView();
-        }
+	ref class Student
+	{
+	public:
+		String^ EnrollmentID;
+		String^ Name;
+		String^ Section;
+	};
 
-    protected:
-        ~DisplayStudents()
-        {
-            if (components)
-            {
-                delete components;
-            }
-        }
+	/// <summary>
+	/// Summary for DisplayStudents
+	/// </summary>
+	public ref class DisplayStudents : public System::Windows::Forms::Form
+	{
+	public:
+		DisplayStudents(void)
+		{
+			InitializeComponent();
+			PopulateDataGridView();
+		}
 
-    private:
-        System::ComponentModel::Container^ components;
-        System::Windows::Forms::DataGridView^ dataGridView1;
-
-        void PopulateDataGridView() {
-            List<Student^>^ students = gcnew List<Student^>();
-
-            String^ filePath = "students.txt";
-            StreamReader^ reader = gcnew StreamReader(filePath);
-
-            String^ line;
-            while ((line = reader->ReadLine()) != nullptr) {
-                Student^ student = gcnew Student();
-                student->EnrollmentID = line;
-                student->Name = reader->ReadLine();
-                student->Section = reader->ReadLine();
-
-                students->Add(student);
-            }
-
-            reader->Close();
-
-            dataGridView1 = gcnew System::Windows::Forms::DataGridView();
-            dataGridView1->Dock = DockStyle::Fill;
-
-            dataGridView1->Columns->Add("EnrollmentID", "Enrollment ID");
-            dataGridView1->Columns->Add("Name", "Name");
-            dataGridView1->Columns->Add("Section", "Section");
-
-            for (int i = 0; i < students->Count; i++)
-            {
-                dataGridView1->Rows->Add(students[i]->EnrollmentID, students[i]->Name, students[i]->Section);
-            }
-
-            this->Controls->Add(dataGridView1);
-        }
-
+	protected:
+		~DisplayStudents()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
 #pragma region Windows Form Designer generated code
-        void InitializeComponent(void)
-        {
-            this->components = gcnew System::ComponentModel::Container();
-            this->Size = System::Drawing::Size(300, 300);
-            this->Text = L"DisplayStudents";
-            this->Padding = System::Windows::Forms::Padding(0);
-            this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-        }
+		void InitializeComponent(void)
+		{
+			this->components = gcnew System::ComponentModel::Container();
+			this->Size = System::Drawing::Size(380, 300);
+			this->Text = L"DisplayStudents";
+			this->Padding = System::Windows::Forms::Padding(0);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		}
 #pragma endregion
-    };
+	private:
+		System::ComponentModel::Container^ components;
+		System::Windows::Forms::DataGridView^ dataGridView1;
+
+		void PopulateDataGridView()
+		{
+			List<Student^>^ students = gcnew List<Student^>();
+
+			String^ connectionString = "Data Source=DESKTOP-MN4CFP4;Initial Catalog=TIMETABLEDB;Integrated Security=True";
+			String^ query = "SELECT EnrollmentID, Name, SectionID FROM Students";
+
+			SqlConnection^ connection = gcnew SqlConnection(connectionString);
+			SqlCommand^ command = gcnew SqlCommand(query, connection);
+
+			try
+			{
+				connection->Open();
+				SqlDataReader^ reader = command->ExecuteReader();
+				while (reader->Read())
+				{
+					Student^ student = gcnew Student();
+					student->EnrollmentID = reader["EnrollmentID"]->ToString();
+					student->Name = reader["Name"]->ToString();
+					student->Section = reader["SectionID"]->ToString();
+
+					students->Add(student);
+				}
+				reader->Close();
+			}
+			catch (Exception^ ex) 
+			{
+				MessageBox::Show("An error occurred while fetching data: " + ex->Message);
+			}
+			finally 
+			{
+				if (connection->State == ConnectionState::Open) 
+				{
+					connection->Close();
+				}
+			}
+
+			dataGridView1 = gcnew System::Windows::Forms::DataGridView();
+			dataGridView1->Dock = DockStyle::Fill;
+
+			dataGridView1->Columns->Add("EnrollmentID", "Enrollment ID");
+			dataGridView1->Columns->Add("Name", "Name");
+			dataGridView1->Columns->Add("Section", "Section");
+
+			for (int i = 0; i < students->Count; i++) 
+			{
+				dataGridView1->Rows->Add(students[i]->EnrollmentID, students[i]->Name, students[i]->Section);
+			}
+
+			this->Controls->Add(dataGridView1);
+		}
+	};
 }
